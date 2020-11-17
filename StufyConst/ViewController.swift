6cd411542ca63874c27c572fc5c535cdbd6d439d
Dutovkb <dutovapple@yandex.ru>
2020-11-17 22:05:23 +0300

    Initial Commit

---
+++ StufyConst/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  StufyConst
+//
+//  Created by Кирилл Дутов on 17.11.2020.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Car Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item .hasPrefix("nssl") {
+                pictures.append(item)
+            }
+    
+        }
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
